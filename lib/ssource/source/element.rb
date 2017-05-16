8be0f25a0be344255a8c5c6852857340248642d3
draveness <i@draveness.me>
2017-05-16 20:35:31 +0800

    feat: add offset/length support for name, body

--- lib/ssource/source/element.rb
+++ lib/ssource/source/element.rb
@@ -2,10 +2,20 @@ module Ssource
   module Source
     class Element
       attr_reader :name, :kind, :accessibility, :elements
+
+      attr_reader :offset, :length, :name_offset, :name_length, :body_offset, :body_length
+
       def initialize(json)
         @name = json['name']
         @accessibility = json['accessibility']
         @kind = json['kind']
+
+        @offset = json['offset']
+        @length = json['length']
+        @name_offset = json['nameoffset']
+        @name_length = json['namelength']
+        @body_offset = json['bodyoffset']
+        @body_length = json['bodylength']
 
         @elements = json.fetch('substructure', []).reduce([]) do |arr, structure|
           arr << Factory.build(structure)
